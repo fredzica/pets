64a771f6bbb13abfd740e0bfc7865ed239af80de
Frederico Zica <fredzica@gmail.com>
2022-05-14 16:09:44 -0300

    storing pets in the state

--- app/javascript/components/pets/Pets.js
+++ app/javascript/components/pets/Pets.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Pets = () => {
-  return <div>This is the Pets view</div>;
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    //retrieve pets and update our state with them
+
+    axios
+      .get("/api/v1/pets")
+      .then((resp) => setPets(resp.data.pets))
+      .catch((resp) => console.log(resp));
+  }, [pets.length]);
+
+  const list = pets.map((pet) => {
+    return <li key={pet.name}>{pet.name}</li>;
+  });
+
+  return (
+    <>
+      <div>This is the Pets view</div>
+      <ul>{list}</ul>
+    </>
+  );
 };
 
 export default Pets;
