89538e06826ca00888b34f7b4398dde1f15941ec
Frederico Zica <fredzica@gmail.com>
2022-05-14 14:25:48 -0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+pets = Pet.create([
+    {
+        name: "Linus",
+        description: "Thin, tall, either sleepy or overexcited",
+        birth_date: "25/12/2019",
+        weight_kg: 24.50
+    },
+    {
+        name: "Nina",
+        description: "Fat, crazy, very playful and always sweating",
+        birth_date: "12/06/2009",
+        weight_kg: 45.0
+    }
+])
