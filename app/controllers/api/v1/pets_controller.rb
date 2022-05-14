218bdc299d5cb03fe1a5cd858f1b6a5a02ce6e90
Frederico Zica <fredzica@gmail.com>
2022-05-14 18:49:27 -0300

    updating and deleting pets

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -7,12 +7,41 @@ class Api::V1::PetsController < ApplicationController
     end
 
     def create
-        pet = Pet.new(params.require(:pet).permit([:name, :description, :birth_date, :weight_kg]))
+        pet = Pet.new(pet_params)
+        if pet.save
+            render json: pet
+        else
+            render json: { error: pet.errors.messages }, status: 422
+        end
+    end
 
-       if pet.save
-        render json: pet
-       else
-        render json: { error: pet.errors.messages }, status: 422
+    def update
+        pet = Pet.find_by(id: params[:id])
+
+        if pet == nil
+            head :not_found
+        elsif pet.update(pet_params)
+            render json: pet
+        else
+            render json: { error: pet.errors.messages }, status: 422
+        end
+    end
+
+    def destroy
+        pet = Pet.find_by(id: params[:id])
+
+        if pet == nil
+            head :not_found
+        elsif pet.delete
+            head :no_content
+        else
+            render json: { error: pet.errors.messages }, status: 422
        end
+    end
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :description, :birth_date, :weight_kg)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :pets, only: [:index, :create]
+      resources :pets, only: [:index, :create, :update, :destroy]
     end
   end
 
