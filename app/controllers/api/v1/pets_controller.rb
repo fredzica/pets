b04ad3418795b80923baf43401966105993509e2
Frederico Zica <fredzica@gmail.com>
2022-05-14 18:20:32 -0300

    API to create pets

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::PetsController < ApplicationController
+    # circumventing CRSF check for now
+    protect_from_forgery with: :null_session
+
     def index
         render json: { pets: Pet.all }
+    end
+
+    def create
+        pet = Pet.new(params.require(:pet).permit([:name, :description, :birth_date, :weight_kg]))
+
+       if pet.save
+        render json: pet
+       else
+        render json: { error: pet.errors.messages }, status: 422
+       end
     end
 end
